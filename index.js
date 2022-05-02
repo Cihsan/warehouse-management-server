50989f38059b4bf7f38628241faf61d3b1dbfa68
Cihsan <mutamuty@gmail.com>
2022-05-03 01:04:36 +0600

    added get and delete operation with mongodb

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express=require('express')
 const app=express()
 const port =process.env.port || 5000
@@ -8,19 +9,35 @@ app.use(express.json()) //for parse
 require('dotenv').config()// for envirment variable
 //goods-store
 //4ucRwGPtCEh0zE02
-app.get('/', (req, res) => {
-  res.send('Welcome To Goods Store Server')
-})
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://goods-store:4ucRwGPtCEh0zE02@cluster0.ug9mt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://react-node-0203:8PeuE982lXgPp8vD@cluster0.lnkho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run(){
     try{
         await client.connect()
         const goodsStore = client.db("goodsDB").collection("goods");
-      
-      
+        //get
+        app.get('/products', async(req,res)=>{
+            const query={}
+            const allProduct=goodsStore.find(query)
+            const product=await allProduct.toArray()
+            res.send(product)
+        })
+        app.get('/products/:id',async (req,res)=>{
+            const id = req.params.id
+            const query={_id:ObjectId(id)}
+            const result= await goodsStore.findOne(query)
+            res.send(result)
+        })
+
+        app.delete('/products/:id', async(req,res)=>{
+            const id= req.params.id
+            const query={_id:ObjectId(id)}
+            const result=await goodsStore.deleteOne(query)
+            res.send(result)
+        })
+        
+        
     }
     finally{
         //await client.close()
@@ -29,6 +46,9 @@ async function run(){
 
 run().catch(console.dir);
 
+app.get('/', (req, res) => {
+    res.send('Welcome To Goods Store Server')
+  })
 
 app.listen(port, () => {
   console.log(`Show Here ${port}`)
