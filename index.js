b3e464e832889252508906845cad4527c28a3af9
Cihsan <mutamuty@gmail.com>
2022-05-02 20:30:46 +0600

    added mongodb connected

--- index.js
+++ index.js
@@ -12,6 +12,24 @@ app.get('/', (req, res) => {
   res.send('Welcome To Goods Store Server')
 })
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://goods-store:4ucRwGPtCEh0zE02@cluster0.ug9mt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+        await client.connect()
+        const goodsStore = client.db("goodsDB").collection("goods");
+      
+      
+    }
+    finally{
+        //await client.close()
+    }
+}
+
+run().catch(console.dir);
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Show Here ${port}`)
 })
