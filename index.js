ab32ff882d75ce7cfc73cf7ea541aa056d88fcea
Cihsan <mutamuty@gmail.com>
2022-05-04 02:06:05 +0600

    added env file

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json()) //for parse
 //goods-store
 //4ucRwGPtCEh0zE02
 
-const uri = "mongodb+srv://react-node-0203:8PeuE982lXgPp8vD@cluster0.lnkho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lnkho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run(){
     try{
